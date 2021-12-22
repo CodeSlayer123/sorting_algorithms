e31184279fd6762f035d102879613959bcc20ce3
CodeSlayer123 <3613@holbertonschool.com>
2021-12-22 10:00:22 -0800

    small change 1

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,49 +1,47 @@
 #include "sort.h"
 /**
- * quick_sort - quick sort algorithm
- * @array: array given to us
- * @size: size of the array
+ * quick_sort - sorts array of integers using quick sort
+ * @array: array
+ * @size: size
+ * Return: nothing
  */
-
 void quick_sort(int *array, size_t size)
 {
+
 	quickSort(array, 0, size - 1, size);
+
+
 }
 
-/**
- * swap - swaps two elements of an array
- * @a: element one
- * @b: element two
- */
+void quickSort(int arr[], int low, int high, size_t size)
+{
+	int pi;
+	if (low < high)
+	{
+		pi = partition(arr, low, high, size);
+		quickSort(arr, low, pi - 1, size);
+		quickSort(arr, pi + 1, high, size);
+	}
+}
 void swap(int *a, int *b)
 {
 	int t = *a;
-
 	*a = *b;
 	*b = t;
 }
-/**
- * partition - quicksort helper
- * @arr: the array given
- * @low: low number
- * @high: high number
- * @size: size of array
- * Return: a pivot
- */
-int partition(int arr[], int low, int high, size_t size)
+int partition (int arr[], int low, int high, size_t size)
 {
 	int pivot = arr[high];
-	int i = (low - 1), j, tmpi, tmpj, tmp;
-
-	for (j = low; j <= high - 1; j++)
+	int i = (low - 1);
+	int tmp;
+	for (int j = low; j <= high- 1; j++)
 	{
 		if (arr[j] <= pivot)
 		{
 			i++;
-			tmpi = arr[i];
-			tmpj = arr[j];
+			tmp = arr[i];
 			swap(&arr[i], &arr[j]);
-			if (tmpi != arr[i] && tmpj != arr[j])
+			if (arr[i] != tmp)
 				print_array(arr, size);
 		}
 	}
@@ -52,22 +50,4 @@ int partition(int arr[], int low, int high, size_t size)
 	if (arr[i + 1] != tmp)
 		print_array(arr, size);
 	return (i + 1);
-}
-/**
- * quickSort - quicksorts
- * @arr: the array
- * @low: low number
- * @high: high number
- * @size: size of array
- */
-void quickSort(int arr[], int low, int high, size_t size)
-{
-	int pi;
-
-	if (low < high)
-	{
-		pi = partition(arr, low, high, size);
-		quickSort(arr, low, pi - 1, size);
-		quickSort(arr, pi + 1, high, size);
-	}
 }
