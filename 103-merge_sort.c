dc0fcc0fa494766a9932e105f0ba71e98b169b38
Code Slayer <3613@holbertonschool.com>
2022-05-05 18:09:27 -0500

    one malloc

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -26,15 +26,15 @@ void merge_sorted_arrays(int *a, int l, int m, int r){
     int right_length = r - m;
 
     int *temp_left = malloc(left_length * sizeof(int));
-    int *temp_right = malloc(right_length * sizeof(int));
+    // int *temp_right = malloc(right_length * sizeof(int));
     int i, j, k;
 
 
     for (i = 0; i < left_length; i++)
         temp_left[i] = a[l + i];
 
-    for (i = 0; i < right_length; i++)
-        temp_right[i] = a[m + 1 + i];
+    // for (i = 0; i < right_length; i++)
+    //    temp_right[i] = a[m + 1 + i];
 
     for (i = 0, j = 0, k = l; k < r; k++)
     {
@@ -42,17 +42,17 @@ void merge_sorted_arrays(int *a, int l, int m, int r){
         print_array(a, 10);
 
 
-        if ((i < left_length) && (j >= right_length || temp_left[i] <= temp_right[j]))
+        if (i < left_length && j >= right_length )
         {
             a[k] = temp_left[i];
             i++;
         }
         else{
-            a[k] = temp_right[j];
+            // a[k] = temp_right[j];
             j++;
         }
     }
     free(temp_left);
-    free(temp_right);
+    // free(temp_right);
 }
 
